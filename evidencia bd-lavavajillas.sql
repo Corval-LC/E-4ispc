9a4469c806a7c360aa29c830654907bffcbab9ce
Corval <150201722+Corval-LC@users.noreply.github.com>
2024-09-08 15:05:17 -0300

    Bd- Evidencia 4 - Lavavajillas

---
+++ evidencia bd-lavavajillas.sql
@@ -0,0 +1,9 @@
+
+
+SELECT * FROM Lavavajillas WHERE estado = 'Nuevo';
+SELECT marca, modelo FROM lavavajillas WHERE capacidad > 40;
+SELECT id_lavavajillas, marca, modelo FROM lavavajillas;
+SELECT * FROM ciclolavado WHERE tipo_programa = 'Intensivo';
+SELECT * FROM ciclolavado WHERE temperatura > 40;
+
+
